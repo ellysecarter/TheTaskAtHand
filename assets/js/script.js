cff2ab3cd6b7e21ccbfd4c3bdacd0b674fdf5423
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2022-02-14 20:56:34 -0800

    tasks can move to in progress or completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content")
 var taskIdCounter = 0;
 
@@ -18,14 +20,34 @@ var taskFormHandler = function (event) {
 
     formEl.reset(); 
 
-    // package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
+    var isEdit = formEl.hasAttribute("data-task-id")
+    if (isEdit){
+        var taskId = formEl.getAttribute("data-task-id")
+        completeEditTask(taskNameInput, taskTypeInput, taskId)
+    }
+        else {
+            var taskDataObj = {
+                name: taskNameInput,
+                type: taskTypeInput
+        }
     };
 
     // send it in as an argument
     createTaskEl(taskDataObj)
+}
+
+var completeEditTask = function(taskName, taskType, taskId){
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id")
+    document.querySelector("#save-task").textContent = "Add Task"
 }
 
 var createTaskEl = function(taskDataObj){
@@ -133,5 +155,28 @@ var deleteTask = function(taskId){
     taskSelected.remove()
 }
 
+var taskStatusChangeHandler = function(event){
+//    get task item ID
+    var taskId = event.target.getAttribute("data-task-id")
+
+    // get current selected options value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase()
+
+    // find parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
+
+    if (statusValue === "to do"){
+        tasksToDoEl.appendChild(taskSelected)
+    } 
+    else if (statusValue === "in progress"){
+        tasksInProgressEl.appendChild(taskSelected)
+    } 
+    else if (statusValue === "completed"){
+        tasksCompletedEl.appendChild(taskSelected)
+    }
+}
+
 pageContentEl.addEventListener("click", taskButtonHandler)
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler)
 
